174b7edf1b0e06a9c35a6aa3954e409dc610af42
Nourhen Meddeb <meddebnourhen21@gmail.com>
2024-09-04 17:47:55 +0100

    first commit

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  padding: 20px;
+`;
+
+const Home = () => (
+  <Container>
+    <h1>Welcome to My Next.js App</h1>
+  </Container>
+);
+
+export default Home;
